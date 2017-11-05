49515494e091046ec61ca10489afa55273341957
Ben Fedidat <fedidat@gmail.com>
2017-11-05 13:10:38 +0200

    split app.js, routes/heroes.js and controller/heroesController.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var heroes = require('./routes/heroes');
 
 var app = express();
 
@@ -24,70 +25,8 @@ app.set('view engine', 'jade');
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/heroes', heroes);
 
-heroes = [
-  {"id": 1, name:"superman"}, 
-  {"id": 2, name:"batman"}, 
-  {"id": 3, name:"green lantern"}
-];
-app.get('/heroes', (req, res) => {
-    res.send(heroes);
-});
-app.get('/heroes/:id', (req, res) => {
-  var hero = heroes.find(hero => hero.id === parseInt(req.params.id));
-  if(!hero)
-    return res.status(404).send("hero not found");
-  return res.send(hero);
-});
-app.put('/heroes/:id', (req, res) => {
-  if(!req.body.id || !req.params.id) {
-    return res.status(400).send("missing id");
-  }
-  if(!req.body.name) {
-    return res.status(400).send("missing name");
-  }
-  if(!heroes.find(hero => hero.id === parseInt(req.params.id))) {
-    return res.status(400).send("hero not found");
-  }
-  if(heroes.find(hero => hero.id === parseInt(req.body.id))) {
-    return res.status(400).send("id already exists");
-  }
-  var heroIndex = heroes.findIndex(hero => hero.id === parseInt(req.params.id));
-  heroes[heroIndex] = {"id": parseInt(req.body.id), "name": req.body.name};
-  return res.send("success");
-});
-app.post('/heroes', (req, res) => {
-  if(!req.body.id) {
-    return res.status(400).send("missing id");
-  }
-  if(heroes.find(hero => hero.id === parseInt(req.body.id))) {
-    return res.status(400).send("hero already exists");
-  }
-  if(!req.body.name) {
-    return res.status(400).send("missing name");
-  }
-  heroes.push({"id": parseInt(req.body.id), "name": req.body.name});
-  return res.send("success");
-});
-app.delete('/heroes/:id', (req, res) => {
-  var index = heroes.findIndex(hero => hero.id === parseInt(req.params.id));
-  if (index < 0) {
-    return res.status(404).send("hero not found");
-  }
-  heroes.splice(index, 1);
-  return res.send("success");
-});
-app.delete('/heroes', (req, res) => {
-  if(!req.query.name) {
-    return res.status(400).send("missing name");
-  }
-  var index = heroes.findIndex(hero => hero.name === parseInt(req.query.name));
-  if (index < 0) {
-    return res.status(404).send("hero not found");
-  }
-  heroes.splice(index, 1);
-  return res.send("success");
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
